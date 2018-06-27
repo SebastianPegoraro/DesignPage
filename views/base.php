c4b6b0f75bd2d25797049a86804d92f06cae2534
SebastianPegoraro <30633725+SebastianPegoraro@users.noreply.github.com>
2018-06-26 21:52:30 -0300

    FirstCommit

---
+++ views/base.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Buho</h1>
+                </div>
+                <div class="col">
+                    <li>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
